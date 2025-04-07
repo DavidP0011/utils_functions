8707e893ee38b8e0df7d1b8bc448f0e6dcc1d090
DavidP <148255648+DavidP0011@users.noreply.github.com>
2025-04-07 10:41:05 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dpm_functions',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # dependencias, ejemplo:
+        'numpy'
+        # etc.
+    ]
+)
