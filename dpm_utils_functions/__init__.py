ed892c77f44083b168db11680933b8c4c1fa488c
DavidP <148255648+DavidP0011@users.noreply.github.com>
2025-04-08 13:54:40 +0200

    Update __init__.py

--- dpm_utils_functions/__init__.py
+++ dpm_utils_functions/__init__.py
@@ -1,1 +1,9 @@
-from dpm_utils_functions.dpm_GCP_ini_utils import _ini_authenticate_API
+# dpm_utils_functions/__init__.py
+
+# Reexporta la función de autenticación
+from .dpm_GCP_ini_utils import _ini_authenticate_API
+
+# Puedes reexportar más funciones o submódulos
+# from .otro_modulo import otra_funcion
+
+__all__ = ["_ini_authenticate_API"]  # Lista de símbolos públicos
